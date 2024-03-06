4ae50f92008998951531707d05a0cc9cd97aa1ae
Francesco Parisi <francescoparisi178@gmail.com>
2024-03-06 15:52:55 +0100

    create new app Management_Prestazioni

--- Healthcare/settings.py
+++ Healthcare/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'smartcontract.apps.SmartcontractConfig',
+    'Management_Prestazioni',
 ]
 
 MIDDLEWARE = [
