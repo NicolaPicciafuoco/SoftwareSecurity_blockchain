bef21045d44892cf4dc83362a39c686694f186a0
Francesco Parisi <francescoparisi178@gmail.com>
2024-03-08 11:03:02 +0100

    overwrite function save

--- Management_Prestazioni/admin.py
+++ Management_Prestazioni/admin.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 from .models import Prestazione
 
 class PrestazioneAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'note', 'file_display')  # Campi da visualizzare nella lista delle prestazioni
+    list_filter = ('note',)  # Filtri laterali per la lista delle prestazioni
+    search_fields = ('note',)  # Campo di ricerca per la lista delle prestazioni
 
+    def file_display(self, obj):
+        if obj.file:
+            return obj.file.name
+        else:
+            return "Nessun file"
+    file_display.short_description = 'File'  # Personalizza l'intestazione della colonna
 
 admin.site.register(Prestazione, PrestazioneAdmin)
--- Management_Prestazioni/models.py
+++ Management_Prestazioni/models.py
@@ -1,16 +1,29 @@
 from django.db import models
+import os
+
+def upload_to_prestazione(instance, filename):
+    return os.path.join('path_prestazione_files', filename)
 
 class Prestazione(models.Model):
-
-    file = models.FileField('Referto', null=True, blank=True)
-
-    note = models.TextField('Note', null=True, blank=True)
+    file = models.FileField('Referto', upload_to=upload_to_prestazione, null=True, blank=True)
+    note = models.TextField('Note', max_length=100, null=True, blank=True)
 
     def __str__(self):
         return f"Prestazione {self.id}: {self.note}"
+
+    def save(self, *args, **kwargs):
+        # Controlla se il campo FileField è stato pulito e il file associato esiste
+        if self.pk:
+            try:
+                old_instance = Prestazione.objects.get(pk=self.pk)
+                if old_instance.file and self.file != old_instance.file:
+                    # Elimina il file precedente se è stato modificato
+                    if os.path.isfile(old_instance.file.path):
+                        os.remove(old_instance.file.path)
+            except Prestazione.DoesNotExist:
+                pass
+        super().save(*args, **kwargs)
+
     class Meta:
-        verbose_name='Prestazione'
-        verbose_name_plural='Prestazioni'
-
-
-
+        verbose_name = 'Prestazione'
+        verbose_name_plural = 'Prestazioni'
