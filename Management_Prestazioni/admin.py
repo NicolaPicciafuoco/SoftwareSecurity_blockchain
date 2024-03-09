5cac8ad1134f8bd90daca5750b5cbfc8eca3e8e7
Francesco Parisi <francescoparisi178@gmail.com>
2024-03-09 18:34:52 +0100

    fix bugs ready to merge for @125ade

--- Management_Prestazioni/admin.py
+++ Management_Prestazioni/admin.py
@@ -1,16 +1,32 @@
+import os
+
 from django.contrib import admin
 from .models import Prestazione
 
 class PrestazioneAdmin(admin.ModelAdmin):
-    list_display = ('id', 'note', 'file_display')  # Campi da visualizzare nella lista delle prestazioni
+    list_display = ('pk', 'user_name', 'operator_name', 'note', 'file_display')  # Aggiunta dei campi 'operator_name'
     list_filter = ('note',)  # Filtri laterali per la lista delle prestazioni
     search_fields = ('note',)  # Campo di ricerca per la lista delle prestazioni
 
+    def user_name(self, obj):
+        if obj.utente:
+            return obj.utente.username
+        else:
+            return "Nessun utente"
+    user_name.short_description = 'Utente'  # Nome visualizzato nell'admin
+
+    def operator_name(self, obj):
+        if obj.operatore:
+            return obj.operatore.username
+        else:
+            return "Nessun operatore"
+    operator_name.short_description = 'Operatore'  # Nome visualizzato nell'admin
+
     def file_display(self, obj):
         if obj.file:
-            return obj.file.name
+            return os.path.basename(obj.file.name)  # Estrae solo il nome del file
         else:
             return "Nessun file"
-    file_display.short_description = 'File'  # Personalizza l'intestazione della colonna
+    file_display.short_description = 'File'  # Nome visualizzato nell'admin
 
 admin.site.register(Prestazione, PrestazioneAdmin)
--- Management_Prestazioni/models.py
+++ Management_Prestazioni/models.py
@@ -1,16 +1,36 @@
+from Healthcare.settings import MEDIA_ROOT
+from django.contrib.auth.models import User
 from django.db import models
 import os
 
 def upload_to_prestazione(instance, filename):
-    return os.path.join('path_prestazione_files', filename)
+    user_id = instance.utente.id if instance.utente else 'default'
+    folder_path = os.path.join(MEDIA_ROOT, 'path_prestazione_files', str(user_id))
+
+    # Assicurati che la cartella esista
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path, exist_ok=True)
+
+    full_path = os.path.join(folder_path, filename)
+    return full_path
 
 class Prestazione(models.Model):
     file = models.FileField('Referto', upload_to=upload_to_prestazione, null=True, blank=True)
     note = models.TextField('Note', max_length=100, null=True, blank=True)
+    utente = models.ForeignKey(User, verbose_name='paziente', related_name='prestazioni_ricevute', on_delete=models.SET_NULL, null=True, blank=True)
+    operatore = models.ForeignKey(User, verbose_name='operatore', related_name='prestazioni_fornite', on_delete=models.SET_NULL, null=True, blank=True)
 
+    def filename(self):
+        if self.file:
+            return os.path.basename(self.file.name)
+        return ''
+
+
+    # Metodo per stampare a schermo le istanze create
     def __str__(self):
-        return f"Prestazione {self.id}: {self.note}"
+        return f"{self.id}, {self.filename()}, {self.utente}"
 
+# overwrite del metodo save per gestire i file
     def save(self, *args, **kwargs):
         # Controlla se il campo FileField è stato pulito e il file associato esiste
         if self.pk:
@@ -22,6 +42,7 @@ class Prestazione(models.Model):
                         os.remove(old_instance.file.path)
             except Prestazione.DoesNotExist:
                 pass
+        # Richiamo il metodo save della classe base per salvare la modifica
         super().save(*args, **kwargs)
 
     class Meta:
