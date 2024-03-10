bc30acd2f5b5df4b7a6c37aabaede17655d5898b
Francesco Parisi <francescoparisi178@gmail.com>
2024-03-10 17:07:33 +0100

    some visual upgrade

--- Management_Prestazioni/admin.py
+++ Management_Prestazioni/admin.py
@@ -4,29 +4,36 @@ from django.contrib import admin
 from .models import Prestazione
 
 class PrestazioneAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'user_name', 'operator_name', 'note', 'file_display')  # Aggiunta dei campi 'operator_name'
+    list_display = ('pk','user_name','operator_name','short_note', 'file_display')  # Aggiunta dei campi 'operator_name'
     list_filter = ('note',)  # Filtri laterali per la lista delle prestazioni
     search_fields = ('note',)  # Campo di ricerca per la lista delle prestazioni
 
     def user_name(self, obj):
         if obj.utente:
-            return obj.utente.username
+            return obj.utente.nome
         else:
             return "Nessun utente"
     user_name.short_description = 'Utente'  # Nome visualizzato nell'admin
 
     def operator_name(self, obj):
         if obj.operatore:
-            return obj.operatore.username
+            return obj.operatore.nome
         else:
             return "Nessun operatore"
     operator_name.short_description = 'Operatore'  # Nome visualizzato nell'admin
 
+
+    def short_note(self, obj):
+        # Mostra solo i primi 20 caratteri della nota
+        return obj.note[:20] if obj.note else 'Nessuna nota'
+    short_note.short_description = 'Note'  # Nome visualizzato nell'admin
+
     def file_display(self, obj):
         if obj.file:
-            return os.path.basename(obj.file.name)  # Estrae solo il nome del file
+            # Restituisce il nome del file senza estensione
+            return os.path.splitext(os.path.basename(obj.file.name))[0]
         else:
             return "Nessun file"
-    file_display.short_description = 'File'  # Nome visualizzato nell'admin
 
+    file_display.short_description = 'File'  # Nome visualizzato nell'admin
 admin.site.register(Prestazione, PrestazioneAdmin)
--- Management_Prestazioni/apps.py
+++ Management_Prestazioni/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ManagementPrestazioniConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Management_Prestazioni'
+    verbose_name = 'gestione prestazioni'
