dc495cb755bb6d3e7f2de2b1c9fe7a04a6eb4e2a
Francesco Parisi <francescoparisi178@gmail.com>
2024-03-12 15:58:39 +0100

    model and admin are ready to merge

--- Management_Prestazioni/admin.py
+++ Management_Prestazioni/admin.py
@@ -1,54 +1,60 @@
+"""Import"""
+import os
 from django.contrib import admin
 from django.utils.html import format_html
-import os
 
 from .models import Prestazione
 
 
 class PrestazioneAdmin(admin.ModelAdmin):
+    """Classe admin per la gestione delle prestazioni"""
     list_display = ('pk', 'user_name', 'operator_name', 'short_note', 'file_display')
     list_filter = ('note',)
     search_fields = ('note',)
     actions = ['delete_model']
 
     def user_name(self, obj):
+        """Metodo che restituisce il nome utente"""
         if obj.utente:
             return obj.utente.nome
-        else:
-            return "Nessun utente"
+        return "Nessun utente"
 
     user_name.short_description = 'Utente'
 
     def operator_name(self, obj):
+        """Metodo che restituisce il nome utente dell'operatore sanitario"""
         if obj.operatore:
             return obj.operatore.nome
-        else:
-            return "Nessun operatore"
+        return "Nessun operatore"
 
     operator_name.short_description = 'Operatore'
 
     def short_note(self, obj):
+        """Metodo che restituisce la nota"""
         return obj.note[:20] if obj.note else 'Nessuna nota'
 
     short_note.short_description = 'Note'
 
     def file_display(self, obj):
+        """Metodo che restituisce il file associato"""
         if obj.file:
             file_url = obj.file.url
             return format_html('<a href="{}" target="_blank">Visualizza</a>', file_url)
-        else:
-            return "Nessun file"
+        return "Nessun file"
 
     file_display.short_description = 'File'
 
     # Togliamo l'azione di default
     def get_actions(self, request):
+        """Metodo per disabilitare la funzione di default"""
         actions = super().get_actions(request)
         if 'delete_selected' in actions:
             del actions['delete_selected']
         return actions
 
+    # Metodo di delete personalizzato
     def delete_model(self, request, obj):
+        """Metodo che elimina le istanze selezionate"""
         # Verifica se obj è un'istanza singola o un insieme di istanze
         if hasattr(obj, '__iter__'):
             # Si tratta di un insieme di istanze
--- Management_Prestazioni/models.py
+++ Management_Prestazioni/models.py
@@ -1,10 +1,14 @@
+"""Import"""
+import os
+
+from django.core.exceptions import ValidationError
+
 from Healthcare.settings import MEDIA_ROOT
 from Management_User.models import HealthCareUser as User
 from django.db import models
-import os
-
 
 def upload_to_prestazione(instance, filename):
+    """Metodo per aggiornare il path"""
     user_id = instance.utente.id if instance.utente else 'default'
     folder_path = os.path.join(MEDIA_ROOT, 'path_prestazione_files', str(user_id))
 
@@ -15,23 +19,24 @@ def upload_to_prestazione(instance, filename):
     full_path = os.path.join(folder_path, filename)
     return full_path
 
-
 class Prestazione(models.Model):
+    """Modello Prestazione"""
     file = models.FileField('Referto', upload_to=upload_to_prestazione, null=True, blank=True)
     note = models.TextField('Note', max_length=100, null=True, blank=True)
     utente = models.ForeignKey(User, verbose_name='paziente', related_name='prestazioni_ricevute',
-                               on_delete=models.SET_NULL, null=True, blank=True)
+                               on_delete=models.SET_NULL, null=True, blank=False)
     operatore = models.ForeignKey(User, verbose_name='operatore', related_name='prestazioni_fornite',
-                                  on_delete=models.SET_NULL, null=True, blank=True)
+                                  on_delete=models.SET_NULL, null=True, blank=False)
 
     def filename(self):
+        """Metodo per leggere il nome del file"""
         if self.file:
             return os.path.basename(self.file.name)
         return ''
 
     # Metodo per stampare a schermo le istanze create
     def __str__(self):
-        return f"{self.id}, {self.filename()}, {self.utente}"
+        return f"{self.pk}, {self.filename()}, {self.utente}"
 
     #  override del metodo delete nella form
     #  DEPRECATA
@@ -43,8 +48,22 @@ class Prestazione(models.Model):
     #         super().delete(*args, **kwargs)
 
     # override del metodo save per gestire i file
-    def save(self, request=None, *args, **kwargs):
 
+    def clean(self):
+        """ sovrascrittura del metodo clean per far uscire gli errori rossi nella form"""
+        super().clean()
+        if self.file:
+            paziente_id = getattr(self.utente, 'id', None)
+            new_file_path = upload_to_prestazione(self, os.path.basename(self.file.name))
+            existing_files = os.listdir(os.path.join(MEDIA_ROOT, 'path_prestazione_files', str(paziente_id)))
+
+            if os.path.basename(new_file_path) in existing_files:
+                raise (ValidationError
+                       ({'file': ['Il file con lo stesso nome esiste già. Scegli un nome diverso.']}))
+
+
+    def save(self, request=None, *args, **kwargs):
+        """Metodo per salvare il file nel path personalizzato per utente"""
         if self.pk:
             try:
                 old_instance = Prestazione.objects.get(pk=self.pk)
@@ -68,5 +87,6 @@ class Prestazione(models.Model):
         super().save(*args, **kwargs)
 
     class Meta:
+        """Definizione dei verbose"""
         verbose_name = 'Prestazione'
         verbose_name_plural = 'Prestazioni'
