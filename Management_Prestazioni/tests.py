8687b5504150cb1582985563b5c35650cd35cdcc
Francesco Parisi <francescoparisi178@gmail.com>
2024-03-12 16:13:09 +0100

    test on Prestazione

--- Management_Prestazioni/tests.py
+++ Management_Prestazioni/tests.py
@@ -1,3 +1,51 @@
 from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from Management_User.models import HealthCareUser as User
+from Management_Prestazioni.models import Prestazione
 
-# Create your tests here.
+class PrestazioneSaveTestCase(TestCase):
+    def setUp(self):
+        # Creiamo un utente di prova
+        self.user = User.objects.create(
+            email='test@example.com',
+            nome='Test',
+            cognome='User',
+            sesso=User.MALE,
+            data_nascita='1990-01-01',
+            luogo_nascita='TestCity',
+            indirizzo_residenza='TestAddress'
+        )
+
+        # Simuliamo un file di test
+        file_content = b'This is a test file content'
+        self.test_file = SimpleUploadedFile("test_file.txt", file_content, content_type="text/plain")
+
+
+    def test_salvataggio_prestazione(self):
+        # Creiamo una Prestazione senza file
+        prestazione = Prestazione(
+            utente=self.user,
+            operatore=self.user,
+            note='Test note'
+        )
+
+        # Salviamo la Prestazione nel database
+        prestazione.save()
+
+        # Verifichiamo che la Prestazione sia stata salvata correttamente
+        self.assertIsNotNone(prestazione.id, "La Prestazione non è stata salvata correttamente nel database")
+
+    def test_salvataggio_file_prestazione(self):
+        # Creiamo una Prestazione con il file di test
+        prestazione = Prestazione.objects.create(
+            utente=self.user,
+            operatore=self.user,
+            file=self.test_file,
+            note='Test note'
+        )
+
+        # Otteniamo il percorso del file salvato
+        file_path = prestazione.file.path
+
+        # Verifichiamo che il file sia stato salvato correttamente
+        self.assertTrue(prestazione.file.storage.exists(file_path), "Il file non è stato salvato correttamente")
