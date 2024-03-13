dc4d66dcb14c59179c979bb69cb425f1052495af
Oathbound <latiosgigetto@gmail.com>
2024-03-13 16:37:31 +0100

    Added broken static files support and collected static files

--- Healthcare/gunicorn_config.py
+++ Healthcare/gunicorn_config.py
@@ -1,11 +1,13 @@
 # gunicorn_config.py
 
 bind = "0.0.0.0:8000"
-module = "Healthcare.wsgi:application"
 
 # workers = 4  # Adjust based on your server's resources
 # worker_connections = 1000
 # threads = 4
 
-certfile = "/etc/letsencrypt/live/your_domain.com/fullchain.pem"
-keyfile = "/etc/letsencrypt/live/your_domain.com/privkey.pem"
+# SSL non funziona. Too bad!
+#
+#certfile = "/etc/letsencrypt/live/healthcareapp.xyz/fullchain.pem"
+#keyfile = "/etc/letsencrypt/live/healthcareapp.xyz/privkey.pem"
+cert_reqs = 1;
--- Healthcare/settings.py
+++ Healthcare/settings.py
@@ -113,7 +113,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -124,3 +125,4 @@ AUTH_USER_MODEL = 'Management_User.HealthCareUser'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
