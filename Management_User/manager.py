83c41b3cf1f6145148d72c48a2feb908d3d6d612
Andrea <24610906+125ade@users.noreply.github.com>
2024-03-26 01:17:57 +0100

    fix db error & fix manage user error & add landing page (index.html)

--- Management_User/manager.py
+++ Management_User/manager.py
@@ -5,28 +5,26 @@ from web3 import Web3
 
 
 class HealthCareUserManager(BaseUserManager):
+    def create_user(self, email, nome, cognome, sesso, data_nascita, luogo_nascita, indirizzo_residenza, password, wallet_address=None,private_key=None, **extra_fields):
+        user = self.model(
+            email=self.normalize_email(email),
+            nome=nome,
+            cognome=cognome,
+            sesso=sesso,
+            data_nascita=data_nascita,
+            luogo_nascita=luogo_nascita,
+            indirizzo_residenza=indirizzo_residenza,
+            is_superuser=False,
+            is_staff=True,
+            is_active=True,
+            wallet_address="ciao",  # Assegna sempre "ciao" come valore predefinito
+            private_key="ciao",
+            **extra_fields
+        )
 
-        def create_user(self, email, nome, cognome, sesso, data_nascita, luogo_nascita, indirizzo_residenza, password,
-                        wallet_address=None,private_key=None, **extra_fields):
-            user = self.model(
-                email=self.normalize_email(email),
-                nome=nome,
-                cognome=cognome,
-                sesso=sesso,
-                data_nascita=data_nascita,
-                luogo_nascita=luogo_nascita,
-                indirizzo_residenza=indirizzo_residenza,
-                is_superuser=False,
-                is_staff=True,
-                is_active=True,
-                wallet_address="ciao",  # Assegna sempre "ciao" come valore predefinito
-                private_key="ciao",
-                **extra_fields
-            )
-
-            user.set_password(password)
-            user.save(using=self._db)
-            return user
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
     def create_superuser(self, email, nome, cognome, sesso, data_nascita, luogo_nascita, indirizzo_residenza, password,
                          **extra_fields):
@@ -45,6 +43,7 @@ class HealthCareUserManager(BaseUserManager):
         )
 
         user.set_password(password)
+        user.save(using=self._db)
         user.groups.add(Group.objects.get_or_create(name=GROUP_AMMINISTRATORE)[0].id)
         user.save(using=self._db)
         return user
--- core/blockchain_utils.py
+++ core/blockchain_utils.py
@@ -1,3 +1,6 @@
+"""
+Raccoglitore di funzioni e classi che servono per la gestione degli smart contract
+"""
 from web3.auto import w3
 
 
@@ -7,3 +10,16 @@ def create_ethereum_wallet():
         'address': account.address,
         'private_key': account.privateKey.hex(),
     }
+
+
+def compile_smart_contract() -> str:
+    """
+    Controlla se il file è compilato oppure no
+    se compilato ritorna il path
+    se non compilato lo compila e ritorna il path
+    """
+    pass
+
+
+def deploy_on_besu(compiled_smart_contract_path: str) -> bool:
+    pass
