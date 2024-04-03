8e8463e991976b807a5ba90378ae7a9e4f71807c
Oathbound <latiosgigetto@gmail.com>
2024-04-03 14:40:31 +0200

    More minor linting fixes

--- contract/deploy.py
+++ contract/deploy.py
@@ -1,12 +1,11 @@
 import json
-
-from web3 import Web3
-
-from solcx import compile_standard, install_solc
 import os
+import time
+from web3 import Web3
+from solcx import compile_standard, install_solc
 from dotenv import load_dotenv
 # from web3.middleware import geth_poa_middleware
-import time
+
 
 
 class ContractInteractions:
@@ -16,20 +15,21 @@ class ContractInteractions:
     chain_id = 1337
     ChainLog = None
 
+    # Loads the already-compiled contract
+
     def load_contract(self):
 
-        # Loads the already-compiled contract
+        # Checks if the compiled code and the contract address files exist
 
         if os.path.exists("./contract/compiled_code.json"):
-            with open("./contract/compiled_code.json", "r") as file:
+            with open("./contract/compiled_code.json", "r", encoding="utf-8") as file:
                 compiled_sol = json.load(file)
             abi = json.loads(compiled_sol["contracts"]["ChainLog.sol"]["ChainLog"]["metadata"])["output"]["abi"]
             self.ChainLog = self.w3.eth.contract(abi=abi)
         if os.path.exists("./contract/contract_address.txt"):
-            with open("./contract/contract_address.txt", "r") as file:
+            with open("./contract/contract_address.txt", "r", encoding="utf-8") as file:
                 contract_address = file.read()
             self.ChainLog = self.w3.eth.contract(address=contract_address, abi=abi)
-            return self.ChainLog
 
     def __init__(self):
         load_dotenv()
@@ -37,10 +37,12 @@ class ContractInteractions:
         self.private_key = os.getenv("ADMIN_PRIVATE_KEY")
         self.load_contract()
 
+    # Function that deploys the contract. Only ran once on startup
+
     def deploy(self):
 
         # Load the contract source code
-        with open("./contract/ChainLog.sol", "r") as file:
+        with open("./contract/ChainLog.sol", "r", encoding="utf-8") as file:
             chain_log_file = file.read()
 
         # We add these two lines that we forgot from the video!
@@ -63,7 +65,7 @@ class ContractInteractions:
             solc_version="0.8.0",
         )
 
-        with open("./contract/compiled_code.json", "w") as file:
+        with open("./contract/compiled_code.json", "w", encoding="utf-8") as file:
             json.dump(compiled_sol, file)
 
         # get bytecode
@@ -88,14 +90,11 @@ class ContractInteractions:
             time.sleep(1)  # Attendi 1 secondo prima di riprovare la connessione
         print("Il nodo si è connesso")
 
-
-        ''' Non ci serve il PoA middleware
-        
-        if self.chain_id == 4:
-            self.w3.middleware_onion.inject(geth_poa_middleware, layer=0)
-            print(self.w3.client_version)
-            
-        '''
+        # Non ci serve il PoA middleware
+        #
+        # if self.chain_id == 4:
+        #     self.w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+        #     print(self.w3.client_version)
 
         # Added print statement to ensure connection succeeded as per
         # https://web3py.readthedocs.io/en/stable/middleware.html#geth-style-proof-of-authority
@@ -128,25 +127,41 @@ class ContractInteractions:
         tx_receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
 
         print(f"Done! Contract deployed to {tx_receipt.contractAddress}")
-        with open("./contract/contract_address.txt", "w") as file:
+        with open("./contract/contract_address.txt", "w", encoding="utf-8") as file:
             file.write(tx_receipt.contractAddress)
 
     # Function to interact with the contract
 
-    def log_action(self, pk, patient, medic, action_type, medic_key, encrypted_data, choice):
+    def log_action(self, pk, patient, medic, action_type, medic_key, hashed_data, choice):
         # Building, signing and sending the transaction
 
         nonce = self.w3.eth.get_transaction_count(medic)
         call_data = None
 
         if action_type == "Create":
-            call_data = self.ChainLog.encodeABI(fn_name="createAction", args=[patient, medic, pk, encrypted_data, choice])
+            call_data = self.ChainLog.encodeABI(fn_name="createAction", args=[patient,
+                                                                              medic,
+                                                                              pk,
+                                                                              hashed_data,
+                                                                              choice])
         elif action_type == "Update":
-            call_data = self.ChainLog.encodeABI(fn_name="updateAction", args=[patient, medic, pk, encrypted_data, choice])
+            call_data = self.ChainLog.encodeABI(fn_name="updateAction", args=[patient,
+                                                                              medic,
+                                                                              pk,
+                                                                              hashed_data,
+                                                                              choice])
         elif action_type == "Delete":
-            call_data = self.ChainLog.encodeABI(fn_name="deleteAction", args=[patient, medic, pk, encrypted_data, choice])
+            call_data = self.ChainLog.encodeABI(fn_name="deleteAction", args=[patient,
+                                                                              medic,
+                                                                              pk,
+                                                                              hashed_data,
+                                                                              choice])
         elif action_type == "Read":
-            call_data = self.ChainLog.encodeABI(fn_name="readAction", args=[patient, medic, pk, encrypted_data, choice])
+            call_data = self.ChainLog.encodeABI(fn_name="readAction", args=[patient,
+                                                                            medic,
+                                                                            pk,
+                                                                            hashed_data,
+                                                                            choice])
 
         # Costruisci la transazione manualmente
         transaction = {
@@ -169,6 +184,8 @@ class ContractInteractions:
 
         return tx_hash.hex()
 
+    # Function to get the action log from the contract
+
     def get_action_log(self, choice):
         # Returns the action log from the contract
         logs = None
@@ -186,6 +203,8 @@ class ContractInteractions:
     #     elif choice == "Prestazione":
     #         action = self.ChainLog.functions.getPrestazioneByKey(key).call()
     #     return action
+
+    # Function to get the desired action based on the given key
     def get_action_by_key(self, key, choice):
         # Returns the log by key
         actions = []
@@ -194,24 +213,3 @@ class ContractInteractions:
         elif choice == "Prestazione":
             actions = self.ChainLog.functions.getPrestazioneByKey(key).call()
         return actions
-
-''' Commentato per ora
-simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
-print(f"Initial Stored Value {simple_storage.functions.retrieve().call()}")
-greeting_transaction = simple_storage.functions.store(15).build_transaction(
-    {
-        "chainId": chain_id,
-        "gasPrice": w3.eth.gas_price,
-        "from": my_address,
-        "nonce": nonce + 1,
-    }
-)
-signed_greeting_txn = w3.eth.account.sign_transaction(
-    greeting_transaction, private_key=private_key
-)
-tx_greeting_hash = w3.eth.send_raw_transaction(signed_greeting_txn.rawTransaction)
-print("Updating stored Value...")
-tx_receipt = w3.eth.wait_for_transaction_receipt(tx_greeting_hash)
-
-print(simple_storage.functions.retrieve().call())
-'''
