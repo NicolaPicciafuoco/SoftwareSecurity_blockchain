3510bd28113d886d09a7f303eb658f686a3f19b7
Andrea <24610906+125ade@users.noreply.github.com>
2024-04-15 15:07:10 +0200

    fix seeds & add istruzioni.txt to show the manual step to implement

--- Healthcare/wsgi.py
+++ Healthcare/wsgi.py
@@ -9,20 +9,13 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-
 from contract.deploy import ContractInteractions as Deploy
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Healthcare.settings')
-makemigrations: str = "python manage.py makemigrations"
-migrate: str = "python manage.py migrate"
 
 # Smart contract creation and deployment, ran once on startup
 deploy_instance = Deploy()
 deploy_instance.deploy()
-
-# inizializza il db
-os.system(makemigrations)
-os.system(migrate)
 
 
 application = get_wsgi_application()
